40482f5d7fc051b6dafb6f17d7526f2f4c917ed7
Suraj Kulkarni <122864683+surajkk93@users.noreply.github.com>
2025-06-21 15:34:37 +0530

    Update nsg main.tf
    added ssh port

--- modules/nsg/main.tf
+++ modules/nsg/main.tf
@@ -31,3 +31,16 @@ resource "azurerm_network_security_rule" "https" {
   network_security_group_name = azurerm_network_security_group.nsg.name
   resource_group_name         = var.rg_name
 }
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "allow-ssh"
+  priority                    = 120
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.rg_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
