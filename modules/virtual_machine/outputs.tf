c0e31ff2d3c1814b77eefb209914a3f5bf65b795
Suraj Kulkarni <122864683+surajkk93@users.noreply.github.com>
2025-06-23 01:24:34 +0530

    Update vm outputs.tf
    Added vm username output

--- modules/virtual_machine/outputs.tf
+++ modules/virtual_machine/outputs.tf
@@ -1,3 +1,9 @@
 output "public_ip" {
   value = azurerm_public_ip.public_ip.ip_address
 }
+
+output "vm_admin_username" {
+  value       = module.vm.admin_username
+  description = "Admin username of the VM"
+}
+
